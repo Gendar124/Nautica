091d8d0c2d040b270b11bec31192fc43d6a1c528
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2025-09-01 16:19:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1609,7 +1609,7 @@ class Document {
       proxyGroupElement += `      <p>Port: ${proxyData.proxyPort}</p>`;
       proxyGroupElement += `      <div id="container-region-check-${i}">`;
       proxyGroupElement += `        <input id="config-sample-${i}" class="hidden" type="text" value="${proxyData.list[0]}">`;
-      proxyGroupElement += `      </div>;
+      proxyGroupElement += `      </div>';
       proxyG_proxy_info += `    </div>`;
       proxyGroupElement += `  </div>`;
       proxyGroupElement += `  <div class="flex flex-col gap-2 mt-4 text-sm">`;
